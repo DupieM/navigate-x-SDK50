9e383d33b0d4aea7acf9a9c45365d64173df8364
DupieM <221326@virtualwindow.co.za>
2024-05-09 12:48:28 +0200

    Bottom Navigation

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Text } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import PhotosScreen from './screens/PhotosScreen';
+import PrivateScreen from './screens/PrivateScreen';
+import DevelopmentScreen from './screens/DevelopmentScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     // Add navigation here
-      <HomeScreen />
+      // <HomeScreen />
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen}/> 
+        <Tab.Screen name="Photos" component={PhotosScreen}/>
+        <Tab.Screen name="Private" component={PrivateScreen}/>
+        <Tab.Screen name="Development" component={DevelopmentScreen}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
